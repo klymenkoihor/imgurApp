4b25194d18f05f33ab0275c55d98e2ec5fcf29aa
Ihor Klymenko <i_klymenko@ukr.net>
2018-07-06 10:58:30 +0300

    first

---
+++ src/imgur_auth.js
@@ -0,0 +1,15 @@
+/*
+Authorization: Client-ID YOUR_CLIENT_ID
+
+Client ID:
+    9cf3738233cc0a9
+
+Client secret:
+    6a3ab0f6978c531457cfc735ba595b55281d52fa
+
+Access Token:
+    a0dcb1cb5e74fac9a22572dc7b9a49a2292d1b05
+
+refresh_token:
+    6b137d8ee326feb0311aa3c23082a28d01c19477
+*/
